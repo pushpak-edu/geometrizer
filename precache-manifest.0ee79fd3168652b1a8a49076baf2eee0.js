ec03e4efa6404192bda1635d69321139d998687e
Pushpak Sharma <pushpaksharma@Pushpaks-Air.ht.home>
2023-02-09 10:55:02 +0530

    Updates

---
+++ precache-manifest.0ee79fd3168652b1a8a49076baf2eee0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fe4ad2eca4ace80c088c5dd821d9292",
+    "url": "/geometrizer/index.html"
+  },
+  {
+    "revision": "2c5bdb0ac109534da060",
+    "url": "/geometrizer/static/css/main.81e6fef5.chunk.css"
+  },
+  {
+    "revision": "b57c39673f650a7a3894",
+    "url": "/geometrizer/static/js/2.6f4f9af9.chunk.js"
+  },
+  {
+    "revision": "2c5bdb0ac109534da060",
+    "url": "/geometrizer/static/js/main.e99451f8.chunk.js"
+  },
+  {
+    "revision": "a2281161f7d492ecda79",
+    "url": "/geometrizer/static/js/runtime~main.684ddaaa.js"
+  }
+]);
